353bb8a7faa47054c4c48a11180f7be7aecc166b
LXZ-RV <thaparavi382@gmail.com>
2026-07-03 23:22:55 +0530

    Add twrp product file

---
+++ twrp_beryl.mk
@@ -0,0 +1,7 @@
+PRODUCT_DEVICE := beryl
+PRODUCT_NAME := twrp_beryl
+PRODUCT_BRAND := Xiaomi
+PRODUCT_MODEL := Redmi Note 14 5G
+PRODUCT_MANUFACTURER := Xiaomi
+
+$(call inherit-product, $(SRC_TARGET_DIR)/product/base.mk)
